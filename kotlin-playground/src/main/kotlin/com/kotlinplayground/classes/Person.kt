a205bcf9faa2abfeb14bf7bc1af08d855b93230a
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-06 17:36:03 -0500

    19. Primary Constructors (#12)

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/classes/Person.kt
@@ -1,12 +1,19 @@
 package org.example.com.kotlinplayground.classes
 
-class Person {
+class Person(
+	val name: String = "",
+	val age: Int = 0
+) {
 	fun action() {
 		println("Person Walks")
 	}
 }
 
 fun main() {
-	val person = Person()
+	val person = Person("Alex", 35)
 	person.action()
+	println("Name: ${person.name} and Age: ${person.age}")
+
+	val person1 = Person()
+	println("Name: ${person1.name} and Age: ${person1.age}")
 }
