50cc3d49578238e63c78311766041affff273886
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-08 13:00:46 -0500

    30. Interfaces - Defining and Overriding Variables

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/interfaces/Interfaces.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/interfaces/Interfaces.kt
@@ -3,6 +3,9 @@ package org.example.com.kotlinplayground.interfaces
 import org.example.com.kotlinplayground.classes.Course
 
 interface CourseRepository {
+
+	var isCoursePersisted: Boolean
+
 	fun getById(id: Int) : Course
 
 	fun save(course: Course) : Int {
@@ -28,8 +31,17 @@ interface B {
 }
 
 class SqlCourseRepository : CourseRepository, Repository {
+
+	override var isCoursePersisted: Boolean = false
+
 	override fun getById(id: Int): Course {
 		return Course(id = id, name = "Reactive Programming in Modern Java using Project Reactor", author = "Dilip")
+	}
+
+	override fun save(course: Course): Int {
+		println("course in SqlCourseRepository: $course")
+		isCoursePersisted = true
+		return super.save(course)
 	}
 
 	override fun getAll(): Any {
@@ -38,12 +50,16 @@ class SqlCourseRepository : CourseRepository, Repository {
 }
 
 class NoSqlCourseRepository : CourseRepository {
+
+	override var isCoursePersisted: Boolean = false
+
 	override fun getById(id: Int): Course {
 		return Course(id = id, name = "Spring Boot in 30 Minutes", author = "Dilip")
 	}
 
 	override fun save(course: Course): Int {
 		println("course in NoSqlCourseRepository: $course")
+		isCoursePersisted = true
 		return super.save(course)
 	}
 }
@@ -64,12 +80,14 @@ fun main() {
 	println("Saved Course Id is: $courseId")
 	val result = sqlCourseRepository.getAll()
 	println("Result is: $result")
+	println("Course persisted value is: ${sqlCourseRepository.isCoursePersisted}")
 
 	val noSqlCourseRepository = NoSqlCourseRepository()
 	val course1 = noSqlCourseRepository.getById(5)
 	println("Course is: $course1")
 	val courseId1 = noSqlCourseRepository.save(course1)
 	println("Saved Course Id is: $courseId1")
+	println("Course persisted value is: ${noSqlCourseRepository.isCoursePersisted}")
 
 	val ab = AB()
 	ab.doSomething()
