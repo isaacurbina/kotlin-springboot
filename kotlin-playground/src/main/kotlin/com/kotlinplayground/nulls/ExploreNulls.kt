81a08088b4d2ee7075e81dad4ab5dd5e6252b8a5
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-12 12:44:40 -0500

    36. Invoking or assigning a Nullable Type to a Non-Nullable Type

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/nulls/ExploreNulls.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/nulls/ExploreNulls.kt
@@ -12,12 +12,24 @@ private fun saveMovie(movie: Movie): Movie {
 	return movie.copy(id = Random.nextInt(0, 100))
 }
 
+private fun printName(name: String) {
+	println("Name is: $name")
+}
+
+private fun printNameNullable(name: String?) {
+	println("Name is: $name")
+}
+
 fun main() {
 	var nameNullable: String? = null
 	println("Value is: ${nameNullable?.length ?: 0}")
+	nameNullable?.run {
+		println(this)
+	}
 
 	nameNullable = "Alex"
 	println("Value is: ${nameNullable.length}")
+	printNameNullable(nameNullable)
 
 	nameNullable = "Dilip"
 	println("Value is: $nameNullable")
