688e86250be959a6ad58b6c2262a384863427ad3
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-14 17:13:20 -0500

    42. FlatMap Operations in Collections

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionsOperations.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/collections/CollectionsOperations.kt
@@ -2,6 +2,7 @@ package org.example.com.kotlinplayground.collections
 
 import org.example.com.kotlinplayground.dataset.Course
 import org.example.com.kotlinplayground.dataset.CourseCategory
+import org.example.com.kotlinplayground.dataset.KAFKA
 import org.example.com.kotlinplayground.dataset.courseList
 
 private fun exploreFilter(
@@ -24,6 +25,16 @@ private fun exploreMap(
 	println("courses: $courses")
 }
 
+private fun exploreFlatMap(courseList: List<Course>, topic: String): List<String> {
+	val topicCourses = courseList.flatMap { course ->
+		val courseName = course.name
+		course.topicsCovered
+			.filter { it == topic }
+			.map { courseName }
+	}
+	return topicCourses
+}
+
 fun main() {
 	val courseList = courseList()
 	val devPredicate = { c: Course -> c.category == CourseCategory.DEVELOPMENT }
@@ -35,4 +46,22 @@ fun main() {
 
 	println("---------------------------------------------------------------------")
 	exploreMap(courseList, devPredicate)
+
+	println("---------------------------------------------------------------------")
+	val list = listOf(listOf(1, 2, 3), listOf(4, 5, 6))
+	val mapResult = list.map { outerList ->
+		outerList.map {
+			it.toDouble()
+		}
+	}
+	println("mapResult: $mapResult")
+	val flatMapResult = list.flatMap { outerList ->
+		outerList.map {
+			it.toDouble()
+		}
+	}
+	println("flatMapResult: $flatMapResult")
+
+	val courses = exploreFlatMap(courseList, KAFKA)
+	println("courses: $courses")
 }
