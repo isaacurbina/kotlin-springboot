a6aaf8e37a38e36020d22afcdbc0e2fc6e7fbb48
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-18 18:19:32 -0500

    49. let Scope Function (#41)

--- kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
+++ kotlin-playground/src/main/kotlin/com/kotlinplayground/scopefunctions/ExploreScopeFunctions.kt
@@ -4,28 +4,46 @@ import org.example.com.kotlinplayground.classes.Course
 import org.example.com.kotlinplayground.classes.CourseCategory
 
 fun main() {
-	exploreApply()
-	exploreAlso()
+    exploreApply()
+    exploreAlso()
+    exploreLet()
 }
 
 private fun exploreApply() {
-	val course = Course(
-		id = 1,
-		name = "Design Thinking in Kotlin",
-		author = "Dilip"
-	).apply {
-		this.courseCategory = CourseCategory.DESIGN
-	}
+    val course = Course(
+        id = 1,
+        name = "Design Thinking in Kotlin",
+        author = "Dilip"
+    ).apply {
+        this.courseCategory = CourseCategory.DESIGN
+    }
 
-	println("course: $course")
+    println("course: $course")
 }
 
 private fun exploreAlso() {
-	Course(
-		id = 1,
-		name = "Design Thinking in Kotlin",
-		author = "Dilip"
-	).also {
-		println("Course is $it")
-	}
+    Course(
+        id = 1,
+        name = "Design Thinking in Kotlin",
+        author = "Dilip"
+    ).also {
+        println("Course is $it")
+    }
+}
+
+private fun exploreLet() {
+    val numbers = mutableListOf(1, 2, 3, 4, 5)
+    val result = numbers.map { it * 2 }.filter { it > 5 }.let {
+        println(it)
+        it.sum()
+    }
+    println(result)
+
+    var name: String? = null
+    name = "Dilip"
+    val result1 = name?.let {
+        println(it)
+        it.uppercase()
+    }
+    println(result1)
 }
