a358cf6ded65817a67553e7893dbf568b0bce702
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-22 14:53:01 -0500

    58. Unit Test for Controller - Using the Mockk Mocking library

--- course-catalog-service/build.gradle.kts
+++ course-catalog-service/build.gradle.kts
@@ -41,6 +41,9 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
 
+	// mockk
+	testImplementation("io.mockk:mockk:1.10.4")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 kotlin {
