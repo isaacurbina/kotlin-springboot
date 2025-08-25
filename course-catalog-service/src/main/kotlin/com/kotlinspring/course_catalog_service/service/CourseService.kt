95990395d2e4a2def2c4a7ce7118636dc61652d1
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-25 13:57:05 -0500

    61. Build the POST Endpoint for adding new Course (#54)

---
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/course_catalog_service/service/CourseService.kt
@@ -0,0 +1,27 @@
+package com.kotlinspring.course_catalog_service.service
+
+import com.kotlinspring.course_catalog_service.dto.CourseDTO
+import com.kotlinspring.course_catalog_service.entity.Course
+import com.kotlinspring.course_catalog_service.repository.CourseRepository
+import mu.KLogging
+import org.springframework.stereotype.Service
+
+@Service
+class CourseService(
+	private val courseRepository: CourseRepository
+) {
+
+	companion object : KLogging()
+
+	fun addCourse(courseDTO: CourseDTO): CourseDTO {
+		val entity = courseDTO.let {
+			Course(null, it.name, it.category)
+		}
+		courseRepository.save(entity)
+		logger.info("Saved course is: $entity")
+
+		return entity.let {
+			CourseDTO(it.id, it.name, it.category)
+		}
+	}
+}
