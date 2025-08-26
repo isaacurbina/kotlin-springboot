a2a838b245a949e70c14fa94c64e3e174fab0784
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-26 13:41:27 -0500

    64. Integration test for the GET endpoint to retrieve all the courses (#57)

--- course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/controller/CourseControllerIntgTest.kt
+++ course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/controller/CourseControllerIntgTest.kt
@@ -1,13 +1,17 @@
 package com.kotlinspring.course_catalog_service.controller
 
 import com.kotlinspring.course_catalog_service.dto.CourseDTO
+import com.kotlinspring.course_catalog_service.repository.CourseRepository
+import com.kotlinspring.course_catalog_service.util.courseEntityList
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -16,6 +20,16 @@ class CourseControllerIntgTest {
 
 	@Autowired
 	lateinit var webTestClient: WebTestClient
+
+	@Autowired
+	lateinit var courseRepository: CourseRepository
+
+	@BeforeEach
+	fun setUp() {
+		courseRepository.deleteAll()
+		val courses = courseEntityList()
+		courseRepository.saveAll(courses)
+	}
 
 	@Test
 	fun addCourse() {
@@ -36,5 +50,21 @@ class CourseControllerIntgTest {
 			.responseBody
 
 		Assertions.assertTrue { savedCourse!!.id != null }
+	}
+
+	@Test
+	fun retrieveAllCourses() {
+		val courses = webTestClient
+			.get()
+			.uri("/v1/courses")
+			.exchange()
+			.expectStatus()
+			.isOk
+			.expectBodyList(CourseDTO::class.java)
+			.returnResult()
+			.responseBody
+
+		println("courseDTOs: $courses")
+		assertEquals(3, courses!!.size)
 	}
 }
