501eede8b07224a202a7d130d8f34f00db7afa30
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-28 17:45:46 -0500

    70. Unit test for the GET Endpoint in CourseController (#63)

--- course-catalog-service/src/test/unit/com/kotlinspring/course_catalog_service/controller/CourseControllerUnitTest.kt
+++ course-catalog-service/src/test/unit/com/kotlinspring/course_catalog_service/controller/CourseControllerUnitTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import kotlin.test.assertEquals
 
 @WebMvcTest(controllers = [CourseController::class])
 @AutoConfigureWebTestClient
@@ -43,5 +44,29 @@ class CourseControllerUnitTest {
 			.responseBody
 
 		Assertions.assertTrue { savedCourse!!.id != null }
+	}
+
+	@Test
+	fun retrieveAllCourses() {
+		every { courseServiceMock.retrieveAllCourses() }.returnsMany(
+			listOf(
+				courseDTO(id = 1),
+				courseDTO(id = 2),
+				courseDTO(id = 3)
+			)
+		)
+
+		val courses = webTestClient
+			.get()
+			.uri("/v1/courses")
+			.exchange()
+			.expectStatus()
+			.isOk
+			.expectBodyList(CourseDTO::class.java)
+			.returnResult()
+			.responseBody
+
+		println("courseDTOs: $courses")
+		assertEquals(3, courses!!.size)
 	}
 }
