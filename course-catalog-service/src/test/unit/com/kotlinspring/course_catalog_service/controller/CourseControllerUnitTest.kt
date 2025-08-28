be85f233aeb4fc49167cde36b2fecd3f1c251476
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-08-28 17:41:56 -0500

    69. Unit test for the Post Endpoint in CourseController (#62)

--- course-catalog-service/src/test/unit/com/kotlinspring/course_catalog_service/controller/CourseControllerUnitTest.kt
+++ course-catalog-service/src/test/unit/com/kotlinspring/course_catalog_service/controller/CourseControllerUnitTest.kt
@@ -1,6 +1,12 @@
 package com.kotlinspring.course_catalog_service.controller
 
+import com.kotlinspring.course_catalog_service.dto.CourseDTO
 import com.kotlinspring.course_catalog_service.service.CourseService
+import com.kotlinspring.course_catalog_service.util.courseDTO
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
@@ -13,6 +19,29 @@ class CourseControllerUnitTest {
 	@Autowired
 	lateinit var webTestClient: WebTestClient
 
-	@Autowired
+	@MockkBean
 	lateinit var courseServiceMock: CourseService
+
+	@Test
+	fun addCourse() {
+		val course = CourseDTO(
+			id = null,
+			name = "Build RestFul APis using SpringBoot and Kotlin",
+			category = "Development"
+		)
+
+		every { courseServiceMock.addCourse(any()) } returns courseDTO(id = 1)
+
+		val savedCourse = webTestClient
+			.post()
+			.uri("/v1/courses")
+			.bodyValue(course)
+			.exchange()
+			.expectStatus().isCreated
+			.expectBody(CourseDTO::class.java)
+			.returnResult()
+			.responseBody
+
+		Assertions.assertTrue { savedCourse!!.id != null }
+	}
 }
