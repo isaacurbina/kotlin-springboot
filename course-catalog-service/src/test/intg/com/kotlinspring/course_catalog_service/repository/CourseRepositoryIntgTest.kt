8cb3b4a5ee2a718b331b3094bb52d6b0512f32c9
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-09-15 20:03:15 -0500

    78. Testing Multiple sets of Data using @Parameterized test

--- course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/repository/CourseRepositoryIntgTest.kt
+++ course-catalog-service/src/test/intg/com/kotlinspring/course_catalog_service/repository/CourseRepositoryIntgTest.kt
@@ -3,9 +3,13 @@ package com.kotlinspring.course_catalog_service.repository
 import com.kotlinspring.course_catalog_service.util.courseEntityList
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 import kotlin.test.Test
 
 @DataJpaTest
@@ -34,5 +38,23 @@ class CourseRepositoryIntgTest {
 		val courses = courseRepository.findCoursesByName("SpringBoot")
 		println("courses: $courses")
 		assertEquals(2, courses.size)
+	}
+
+	@ParameterizedTest
+	@MethodSource("courseAndSize")
+	fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+		val courses = courseRepository.findCoursesByName(name)
+		println("courses: $courses")
+		assertEquals(expectedSize, courses.size)
+	}
+
+	companion object {
+		@JvmStatic
+		fun courseAndSize(): Stream<Arguments> {
+			return Stream.of(
+				Arguments.arguments("SpringBoot", 2),
+				Arguments.arguments("Wiremock", 1)
+			)
+		}
 	}
 }
