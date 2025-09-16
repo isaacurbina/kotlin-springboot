95726a92e46318d64a0c470ad5d5dc8a18fe1338
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-09-16 17:38:59 -0500

    83_Instructor-Controller-to-Manage-Instructor-Data (#76)

---
+++ course-catalog-service/src/main/kotlin/com/kotlinspring/course_catalog_service/repository/InstructorRepository.kt
@@ -0,0 +1,7 @@
+package com.kotlinspring.course_catalog_service.repository
+
+import com.kotlinspring.course_catalog_service.entity.Instructor
+import org.springframework.data.repository.CrudRepository
+
+
+interface InstructorRepository : CrudRepository<Instructor, Int>
